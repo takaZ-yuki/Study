eb55689c187df6be61296151958e99b72b45c9d6
yuki <gulgul.groove@gmail.com>
2017-01-28 02:29:28 +0900

    htmlもビルド設定に追加（まだ理解できていない）

--- webpack-dev-server/section1/webpack.config.js
+++ webpack-dev-server/section1/webpack.config.js
@@ -5,9 +5,13 @@ module.exports = {
     context: path.join(__dirname, 'src'),
     entry: {
         javascript: './app.js',
+        html: './index.html'
     },
 
-    output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
 
     // Configuration for dev server
     devServer: {
@@ -15,16 +19,22 @@ module.exports = {
         port: 3000
     },
 
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: {
-                    presets:['es2015']
-                }
-            }
-        ]
-    }
-}
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query:{
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'file?name=[path][name].[ext]'
+      }
+    ]
+  }
+};
